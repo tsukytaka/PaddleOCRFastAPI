a4f2ede90f1fa2e346c0f7bb7ec06aa6711726dc
Long Nguyen <long.nm@anlab.vn>
2024-09-18 12:10:39 +0700

    process multi model

--- models/OCRModel.py
+++ models/OCRModel.py
@@ -43,9 +43,10 @@ class ImageReader():
         # kwargs['model']['charset_test'] = "0123456789"
         # print(kwargs)
         # print(f'Additional keyword arguments: {kwargs}')
-
+        self.model_plate_no = load_from_checkpoint('parseq_rec_model/parseq_plate_no_2024_09_13.ckpt', **kwargs).eval().to(self.args.device)
         self.model = load_from_checkpoint('parseq_rec_model/parseq-2024_05_19.ckpt', **kwargs).eval().to(self.args.device)
         self.model_writer_1 = load_from_checkpoint('parseq_rec_model/parseq_writer_1.ckpt', **kwargs).eval().to(self.args.device)
+        print(f'model_writer_1: parseq_rec_model/parseq_writer_1.ckpt')
         self.img_transform = SceneTextDataModule.get_transform(self.model.hparams.img_size)
 
     def ProcessImage(self, imageFileBytes, modelType):
@@ -162,6 +163,23 @@ class ImageReader():
         im_show.save(bytes_image, format='PNG')
         return bytes_image.getvalue()
     
+    def postprocess_prediction(self, prediction, part3_max_length):
+        """Postprocess the model's prediction."""
+        if prediction[0] == "D":
+            formats = [7, 2, part3_max_length]
+            parts = prediction.split('-')
+            
+            if len(parts) != 3:
+                parts = [parts[0], parts[1][:2], parts[1][2:]]
+            
+            for i in range(1, len(formats)):
+                parts[i] = parts[i][:formats[i]]
+            
+            parts[1] = parts[1].replace('/', '1')
+            parts[2] = parts[2].replace('/', '1') if len(parts[2]) == 3 else parts[2]
+            
+            return "-".join(parts)
+        return prediction
 
     def ReadImageWithPos(self, imageFileBytes, items, modelType):
         img = bytes_to_ndarray(imageFileBytes)
@@ -171,12 +189,36 @@ class ImageReader():
         scores = [] 
         images=[]
         origBoxes = []
+        plateNoImgs = []
         #rec by parseq
+        plateNoItems = []
+        valueItems = []
         for i in range(len(items)):
             x,y,w,h = items[i]["position"]
             origBoxes.append([x,y,x+w,y+h])
             textImg = orgImg[origBoxes[i][1]:origBoxes[i][3], origBoxes[i][0]:origBoxes[i][2]]
-            images.append(self.img_transform(Image.fromarray(textImg, 'RGB')))
+            if items[i]["title"] == "plateNo":
+                plateNoImgs.append(self.img_transform(Image.fromarray(textImg, 'RGB')))
+                plateNoItems.append(items[i])
+            else:
+                images.append(self.img_transform(Image.fromarray(textImg, 'RGB')))
+                valueItems.append(items[i])
+
+        if len(plateNoImgs) > 0:
+            plateNoImgs = torch.stack(plateNoImgs).to(self.args.device)
+            with torch.no_grad():
+                p = self.model_plate_no(plateNoImgs)
+                p =  torch.softmax(p, dim=2)
+                pred, p = self.model_plate_no.tokenizer.decode(p)
+                txts = pred
+                scores = ([s.cpu().mean().item() for s in p])
+                for i in range(len(txts)):
+                    txt = txts[i]
+                    txt = self.postprocess_prediction(txt,9)
+                    txt = re.sub("[,.]", "", txt)
+                    plateNoItems[i]["content"] =txt
+                    plateNoItems[i]["score"] =scores[i]
+
         if len(images) > 0:
             images = torch.stack(images).to(self.args.device)
             with torch.no_grad():
@@ -187,7 +229,7 @@ class ImageReader():
                 p[:, :, 77:] = 0
                 if modelType == 0:
                     pred, p = self.model.tokenizer.decode(p)
-                if modelType == 1:
+                elif modelType == 1:
                     pred, p = self.model_writer_1.tokenizer.decode(p)
                 txts = pred
                 scores = ([s.cpu().mean().item() for s in p])
@@ -196,8 +238,8 @@ class ImageReader():
                     txt = re.sub(",", ".", txt)
                     txt = re.sub("(^\\D+)","", txt)
                     txt = re.sub("(\\D+$)","", txt)
-                    items[i]["content"] =txt
-                    items[i]["score"] =scores[i]
+                    valueItems[i]["content"] =txt
+                    valueItems[i]["score"] =scores[i]
 
 
         drawImg = drawResult(drawImg, origBoxes, txts)
@@ -205,4 +247,5 @@ class ImageReader():
         im_show = Image.fromarray(array, mode="RGB")
         bytes_image = io.BytesIO()
         im_show.save(bytes_image, format='PNG')
+        items = plateNoItems + valueItems
         return items, bytes_image.getvalue()
