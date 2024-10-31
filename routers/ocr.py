90a6fa82644ca58657ab8f3d209e31587e17230e
Long Nguyen <long.nm@anlab.vn>
2024-10-31 09:06:54 +0700

    rollback api interface

--- routers/ocr.py
+++ routers/ocr.py
@@ -17,11 +17,11 @@ ocr = PaddleOCR(use_angle_cls=True, lang=OCR_LANGUAGE)
 imageReader = ImageReader()
 
 @router.post('/predict-by-file', response_model=RestfulModel, summary="read file")
-async def predict_by_file(file: UploadFile, infos: str=Form(), model:int=1):
+async def predict_by_file(file: UploadFile, model:int=1):
     # imageReader: ImageReader = ImageReader()
     # restfulModel: RestfulModel = RestfulModel()
     if file.filename.endswith((".jpg", ".jpeg",".png",".JPG", ".JPEG",".PNG")):
-        configs = json.loads(infos)
+        configs = {}
         # restfulModel.resultcode = 200
         # restfulModel.message = file.filename
         file_data = file.file
@@ -43,26 +43,26 @@ async def predict_by_file(file: UploadFile, infos: str=Form(), model:int=1):
 
     return Response(output_file_bytes, media_type="image/png")
 
-@router.post('/read-file-with-position', response_model=RestfulModel, summary="read file at some position")
-async def read_file_with_position(file: UploadFile, positions: str=Form(), model:int=0): 
-    # restfulModel: RestfulModel = RestfulModel()
+# @router.post('/read-file-with-position', response_model=RestfulModel, summary="read file at some position")
+# async def read_file_with_position(file: UploadFile, positions: str=Form(), model:int=0): 
+#     # restfulModel: RestfulModel = RestfulModel()
     
-    print("file name: ",file.filename)
-    # print("positions: ",positions)
-    # test_array = [{"tableNo":1,"rowNo":1,"colNo":1,"content":36,"position":[601,796,79,42]}]
-    # positions = json.dumps(test_array)
-    items = json.loads(positions)
-    print("items: ", items)
-    if file.filename.endswith((".jpg", ".jpeg",".png",".JPG", ".JPEG",".PNG")):
-        file_data = file.file
-        file_bytes = file_data.read()
-        output_file_bytes = file_bytes
-        items, output_file_bytes = imageReader.ReadImageWithPos(file_bytes, items, model)
-        print("items: ", items)
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="only upload file .jpg, .jpeg or .png"
-        )
-    return Response(json.dumps(items))
-    # return Response(output_file_bytes, media_type="image/png")
+#     print("file name: ",file.filename)
+#     # print("positions: ",positions)
+#     # test_array = [{"tableNo":1,"rowNo":1,"colNo":1,"content":36,"position":[601,796,79,42]}]
+#     # positions = json.dumps(test_array)
+#     items = json.loads(positions)
+#     print("items: ", items)
+#     if file.filename.endswith((".jpg", ".jpeg",".png",".JPG", ".JPEG",".PNG")):
+#         file_data = file.file
+#         file_bytes = file_data.read()
+#         output_file_bytes = file_bytes
+#         items, output_file_bytes = imageReader.ReadImageWithPos(file_bytes, items, model)
+#         print("items: ", items)
+#     else:
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#             detail="only upload file .jpg, .jpeg or .png"
+#         )
+#     return Response(json.dumps(items))
+#     # return Response(output_file_bytes, media_type="image/png")
